f545bf2e41ab7c28efc5906e72e8c69448ec4756
Samuel Wellander <wellander8@gmail.com>
2017-01-19 17:26:46 -0800

    configure passport, add sign up routes

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
- var express       = require("express"),
-    app            = express(),
-    bodyParser     = require("body-parser"),
-    mongoose       = require("mongoose"),
-    Campground     = require("./models/campground"),
-    seedDB         = require("./seeds"),
-    Comment        = require("./models/comments")
+ var express                  = require("express"),
+     app                       = express(),
+     bodyParser                = require("body-parser"),
+     mongoose                  = require("mongoose"),
+     Campground                = require("./models/campground"),
+     seedDB                    = require("./seeds"),
+     Comment                   = require("./models/comments"),
+     passport                  = require("passport"),
+     LocalStrategy             = require("passport-local"),
+     passportLocalMongoose     = require("passport-local-mongoose"),
+     User                      = require("./models/user")
+
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("veiw engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+//PASSPORT CONFIG
 
+app.use(require("express-session")({
+  secret: "Keep it safe",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser);
+passport.deserializeUser(User.deserializeUser);
 
 
 
@@ -127,7 +142,26 @@ app.post("/campgrounds/:id/comments", function(req, res){
 
 });
 
+//AUTH ROUTES
 
+//show register form
+app.get("/register", function(req, res){
+  res.render("register.ejs");
+});
+
+//handle sign up logic
+app.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("/register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/campgrounds");
+    });
+  });
+});
 app.listen(3002, function() {
   console.log("server launched on port 3002");
 });
